fdbd64dddedccd2c0dcfc5d8ddd93cb157d5a5dd
monkeybutter <Csiro2012>
2014-02-25 10:48:13 +1100

    v1.0.2 Flask/D3 basic structure created

---
+++ web/app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    Flask
+    ~~~~~~
+    2014 by CSIRO.
+"""
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
